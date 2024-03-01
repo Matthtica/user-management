5fc6d0c7d64cdbe11497703f56f1bd7ed58d826e
Matthtica <zxnightstalker199733@gmail.com>
2024-03-01 10:38:22 +0630

    Demo using the side drawer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
+'use client'
+import React from "react";
 import RawMdiAlarmOff from '~icons/mdi/alarm-off';
-import Cpp from '~icons/vscode-icons/file-type-cpp3';
-import { SideDrawer } from "@/components/custom/SideDrawer";
-import { Button } from '@/components/ui/button'
+import UserThreeLight from '~icons/ph/users-three';
+import { SideDrawer, SideButton } from "@/components/custom/SideDrawer";
 
 export default function Home() {
-  return <div className="w-100 h-screen">
-    <SideDrawer />
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return <div className="w-100 h-screen flex">
+    <SideDrawer isOpen={isOpen} setIsOpen={setIsOpen}>
+      <SideButton isOpen={isOpen} text="Alarm Off">
+        <RawMdiAlarmOff />
+      </SideButton>
+      <SideButton isOpen={isOpen} text="User">
+        <UserThreeLight />
+      </SideButton>
+    </SideDrawer>
   </div>
 }
