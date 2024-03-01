f687fbc12e858769bf2f46dbe018510fa9498c9f
Matthtica <zxnightstalker199733@gmail.com>
2024-03-01 22:42:33 +0630

    TanstackQuery added and setted up

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import UserRoleSetting from '~icons/clarity/administrator-line'
 import HomeOutline from '~icons/ant-design/home-outlined'
 import { SideDrawer, SideButton } from "@/components/custom/SideDrawer";
 import { useRouter } from "next/navigation";
+import TanstackQueryClientProvider from "@/lib/tanstack-queryclient-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,20 +29,22 @@ export default function RootLayout({
           defaultTheme="light"
           enableSystem
         >
-          <SideDrawer isOpen={isOpen} setIsOpen={setIsOpen}>
-            <SideButton isOpen={isOpen} text="Home" onClick={() => router.push('/')}>
-              <HomeOutline />
-            </SideButton>
-            <SideButton isOpen={isOpen} text="User" onClick={() => router.push('/users')}>
-              <UserThreeLight />
-            </SideButton>
-            <SideButton isOpen={isOpen} text="Role" onClick={() => router.push('/roles')}>
-              <UserRoleSetting />
-            </SideButton>
-          </SideDrawer>
-          <div className="flex-1 m-2 bg-background rounded-md overflow-hidden shadow-md">
-            {children}
-          </div>
+          <TanstackQueryClientProvider>
+            <SideDrawer isOpen={isOpen} setIsOpen={setIsOpen}>
+              <SideButton isOpen={isOpen} text="Home" onClick={() => router.push('/')}>
+                <HomeOutline />
+              </SideButton>
+              <SideButton isOpen={isOpen} text="User" onClick={() => router.push('/users')}>
+                <UserThreeLight />
+              </SideButton>
+              <SideButton isOpen={isOpen} text="Role" onClick={() => router.push('/roles')}>
+                <UserRoleSetting />
+              </SideButton>
+            </SideDrawer>
+            <div className="flex-1 m-2 bg-background rounded-md overflow-hidden shadow-md">
+              {children}
+            </div>
+          </TanstackQueryClientProvider>
         </ThemeProvider>
       </body>
     </html>
