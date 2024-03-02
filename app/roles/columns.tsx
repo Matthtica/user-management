13415c43bd19a4269af2c986d0077a3c5d2b3731
Matthtica <zxnightstalker199733@gmail.com>
2024-03-02 21:42:10 +0630

    ColumnDef defined to display permission properly

--- app/roles/columns.tsx
+++ app/roles/columns.tsx
@@ -1,17 +1,63 @@
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import { type Role } from "@/lib/db/schema";
+import { deserializeToDisplayString } from "@/lib/db/utils";
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Role>[] = [
   {
     accessorKey: 'name',
-    header: "Name",
+    header: ({ column }) => {
+      return <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+        Name
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    },
   },
   {
     accessorKey: 'products_permission',
     header: "Product Permission",
+    cell: ({ row }) => {
+      const permission = row.original.productPermission;
+      return deserializeToDisplayString(permission);
+    }
   },
   {
     accessorKey: 'workspace_permission',
     header: "Workspace Permission",
+    cell: ({ row }) => {
+      const permission = row.original.workspacePermission;
+      return deserializeToDisplayString(permission);
+    }
+  }
+]
+
+export const mock_data: Role[] = [
+  {
+    id: 1,
+    name: 'Admin',
+    productPermission: 'T|T|T|T',
+    workspacePermission: 'T|T|T|T'
+  },
+  {
+    id: 2,
+    name: 'Customer',
+    productPermission: 'F|T|F|F',
+    workspacePermission: 'F|T|F|F'
+  },
+  {
+    id: 3,
+    name: 'Manager',
+    productPermission: 'T|T|T|F',
+    workspacePermission: 'F|T|T|F',
+  },
+  {
+    id: 4,
+    name: 'Sale Person',
+    productPermission: 'F|T|T|F',
+    workspacePermission: 'F|T|T|F'
   }
 ]
