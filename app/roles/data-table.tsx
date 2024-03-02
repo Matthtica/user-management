1ddeac4fa0c109b2a623cff34dcd0cc48443ac60
Matthtica <zxnightstalker199733@gmail.com>
2024-03-02 21:43:18 +0630

    DataTable is now sortable via name

--- app/roles/data-table.tsx
+++ app/roles/data-table.tsx
@@ -2,8 +2,10 @@
 
 import {
   ColumnDef,
+  SortingState,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 
@@ -15,6 +17,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import React from "react"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -24,14 +27,21 @@ interface DataTableProps<TData, TValue> {
 export function DataTable<TData, TValue>({
   columns, data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   })
 
   return (
-    <div className="rounded-md border">
+    <div className="rounded-md border shadow-md">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
