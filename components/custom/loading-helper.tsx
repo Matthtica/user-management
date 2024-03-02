61c328860f5665ee3e30408552fd6ccd6f75423f
Matthtica <zxnightstalker199733@gmail.com>
2024-03-02 23:51:23 +0630

    Loading should be large and in center

--- components/custom/loading-helper.tsx
+++ components/custom/loading-helper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ThreeDotsLoading from '~icons/eos-icons/three-dots-loading';
+import BubbleLoading from '~icons/eos-icons/bubble-loading';
 
 interface LoadingHelperProps extends React.HTMLAttributes<HTMLDivElement> {
   isPending: boolean,
@@ -9,7 +9,7 @@ interface LoadingHelperProps extends React.HTMLAttributes<HTMLDivElement> {
 // TODO: Loading animation
 // TODO: Error message red and big like serious error
 export default function LoadingHelper( { isPending, error, children, ...props }: LoadingHelperProps) {
-  const LoadingComp = <ThreeDotsLoading className="w-12 h-12" />
+  const LoadingComp = <BubbleLoading className="w-10 h-10 mx-auto mt-40" />
 
   return <div {...props}>
     {isPending ? LoadingComp : error ? error.message : children}
