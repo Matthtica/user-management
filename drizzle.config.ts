e81fce0163c32de520c5ffc4b3bd37a4275fc626
Matthtica <zxnightstalker199733@gmail.com>
2024-03-02 21:40:32 +0630

    Bug fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,6 @@ export default {
   driver: 'pg',
   dbCredentials: {
     host: process.env.DATABASE_HOST as string,
-    port: parseInt(process.env.DATABASE_PORT as string),
     user: process.env.DATABASE_USER as string,
     password: process.env.DATABASE_PASSWORD as string,
     database: process.env.DATABASE_NAME as string,
