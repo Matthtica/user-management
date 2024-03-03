e4e1d92a3b07862808d8cd2c08f5fa36e624bbd8
Matthtica <zxnightstalker199733@gmail.com>
2024-03-03 09:19:41 +0630

    Removed unused client

--- lib/db/migration.ts
+++ lib/db/migration.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config'
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { db, client } from './index';
+import {
+  db,
+  //client
+} from './index';
 
 await migrate(db, { migrationsFolder: './drizzle' });
-await client.end();
+//await client.end();
