b04d80c82a729128cce7a1d57dd272bd33cc62d4
Matthtica <zxnightstalker199733@gmail.com>
2024-03-03 09:05:31 +0630

    escaping `'` with &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ export default function Home() {
     <h1 className="text-6xl font-bold">
       Wellcome to Pico SBS
     </h1>
-    <h2 className="text-xl">The most feature complete ERP solution, and it's cheap!!!</h2>
+    <h2 className="text-xl">The most feature complete ERP solution, and it&apos;s cheap!!!</h2>
   </div>
 }
