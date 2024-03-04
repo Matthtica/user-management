468d66c6f1b3d8874863ce45d4981bc48d7c80c2
Matthtica <zxnightstalker199733@gmail.com>
2024-03-04 17:32:52 +0630

    Role page's Table now properly scrollable

--- app/roles/page.tsx
+++ app/roles/page.tsx
@@ -15,7 +15,7 @@ const Roles: FC = () => {
       .catch((err: any) => console.log(err)),
   });
 
-  return <div className="m-5 flex flex-col gap-3">
+  return <div className="flex-1 m-5 flex flex-col gap-3 overflow-y-hidden">
     <div className="flex items-center justify-between">
       <h1 className="text-3xl font-bold">Roles Manager</h1>
       <RoleEntryFormDialog refetch={refetch}/>
@@ -23,7 +23,7 @@ const Roles: FC = () => {
     <FetchLoading
       className="mx-auto mt-40 w-10 h-10"
       isPending={isPending} error={error}>
-      <DataTable columns={columns} data={data!} />
+      <DataTable columns={columns} data={data!} className="overflow-y-scroll"/>
     </FetchLoading>
   </div>
 }
