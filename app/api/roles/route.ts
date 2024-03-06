4f6230d4bf8011e63101fdbb30b5fa053e05aab4
Matthtica <zxnightstalker199733@gmail.com>
2024-03-06 19:42:42 +0630

    Now handle error with ResponseToastMessage for now.

--- app/api/roles/route.ts
+++ app/api/roles/route.ts
@@ -1,23 +1,65 @@
 import { db } from "@/lib/db";
-import { role } from "@/lib/db/schema";
-import { RoleRestType } from "@/lib/typedefs/REST-types";
+import { roles, users  } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { RoleRestType, ResponseToastMessage } from "@/lib/typedefs/rest-types";
 import { serialize_permission } from "@/lib/db/utils";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  console.log("inside /api/role POST route")
-  const data: RoleRestType = await request.json();
+
+export async function POST(req: NextRequest) {
+  console.log("inside /api/roles POST route")
+  const data: RoleRestType = await req.json();
   console.log(data);
 
-  await db.insert(role).values({
+  const result = await db.insert(roles).values({
     name: data.name,
     productPermission: serialize_permission(data.productPermission),
     workspacePermission: serialize_permission(data.workspacePermission),
-  });
+  }).returning();
+
+  if (result.length === 0) {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Invalid",
+      description: "Cannot add role"
+    });
+  } else {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Success",
+      description: "New role created"
+    });
+  }
 }
 
-export async function GET(_: Request) {
-  console.log("inside /api/role GET route")
+export async function GET() {
+  console.log("inside /api/roles GET route")
   // TODO: Can do pagination, with page param
-  const result = await db.select().from(role);
-  return Response.json(result);
+  const result = await db.select().from(roles);
+  return NextResponse.json(result);
+}
+
+export async function DELETE(req: NextRequest) {
+  console.log("inside /api/roles DELETE route")
+  const id: number = await req.json();
+
+  const related_users = await db.select().from(users).where(eq(users.roleId, id));
+
+  if (related_users.length > 0) {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Cannot delete role",
+      description: "This role is associated with some users"
+    });
+  } else {
+    const result = await db.delete(roles).where(eq(roles.id, id)).returning();
+    if (result.length === 0) {
+      return NextResponse.json<ResponseToastMessage>({
+        title: "Invalid",
+        description: "No such role exists"
+      });
+    } else {
+      return NextResponse.json<ResponseToastMessage>({
+        title: "Success",
+        description: "A role has been deleted",
+      });
+    }
+  }
 }
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,19 +1,63 @@
 import { db } from "@/lib/db";
 import { users } from "@/lib/db/schema";
-import { UserRestType } from "@/lib/typedefs/REST-types";
+import { eq } from "drizzle-orm";
+import { UserRestType, ResponseToastMessage } from "@/lib/typedefs/rest-types";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST(req: Request) {
   console.log("inside /api/users POST route")
-  const {name, email, roleId }: UserRestType = await request.json();
+  const {name, email, roleId }: UserRestType = await req.json();
+
+  const existing_user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email));
+
+  if (existing_user.length !== 0) {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Invalid",
+      description: "User with this email already exists"
+    });
+  }
+
   const user = await db
     .insert(users)
     .values({ name, email, roleId })
     .returning()
-  return Response.json(user[0]);
+
+  if (user.length === 0) {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Invalid",
+      description: "Cannot add user"
+    });
+  } else {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Success",
+      description: "New user has been added"
+    });
+  }
 }
 
-export async function GET(_: Request) {
+export async function GET() {
   console.log("inside /api/users GET route")
   const result = await db.select().from(users);
   return Response.json(result);
+}
+
+export async function DELETE(req: NextRequest) {
+  console.log("inside /api/users DELETE route")
+  const id = await req.json();
+  const result = await db.delete(users).where(eq(users.id, id)).returning();
+
+  if (result.length === 0) {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Invalid",
+      description: "No such user exists"
+    });
+  } else {
+    return NextResponse.json<ResponseToastMessage>({
+      title: "Success",
+      description: "A user has been deleted",
+    });
+  }
 }
