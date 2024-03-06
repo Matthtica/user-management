faae62cdf63cfd77cbc9920251eb70b35f701f84
Matthtica <zxnightstalker199733@gmail.com>
2024-03-06 15:06:56 +0630

    Both pages now use custom hooks and adapted to new datatable

--- app/roles/page.tsx
+++ app/roles/page.tsx
@@ -1,21 +1,13 @@
 'use client'
 import React from 'react'
-import { useQuery } from '@tanstack/react-query'
 import { FetchLoading } from '@/components/custom/loading-helper'
 import { columns } from './components/columns'
 import DataTable from '@/components/custom/data-table'
-import { type Role } from '@/lib/db/schema';
 import RoleEntryFormDialog from './components/role-entry-form-dialog'
-import { staleTime } from '@/lib/constants'
+import { useRoles } from '@/lib/hooks'
 
 export default function Roles() {
-  const { isPending, error, data, refetch } = useQuery<Role[]>({
-    queryKey: ['roles'],
-    queryFn: () => fetch('/api/roles')
-      .then((res: Response) => res.json())
-      .catch((err: any) => console.log(err)),
-    staleTime,
-  });
+  const { isPending, isFetching, error, data, refetch } = useRoles();
 
   return <div className="flex-1 m-5 flex flex-col gap-3 overflow-y-hidden">
     <div className="flex items-center justify-between">
@@ -25,7 +17,7 @@ export default function Roles() {
     <FetchLoading
       className="mx-auto mt-40 w-10 h-10"
       isPending={isPending} error={error}>
-      <DataTable columns={columns} data={data!} className="overflow-y-scroll"/>
+      <DataTable columns={columns} isLoading={isFetching} data={data!} className="overflow-y-scroll"/>
     </FetchLoading>
   </div>
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,74 +3,21 @@ import React from 'react'
 import DataTable from '@/components/custom/data-table'
 import { FetchLoading } from '@/components/custom/loading-helper'
 import { columns } from './components/columns';
-import { type User, type Role } from '@/lib/db/schema';
-import { useQuery } from '@tanstack/react-query'
-import UserEntryFormDialog, { type RoleDisplayMap } from './components/user-entry-form-dialog'
-import { staleTime } from '@/lib/constants';
-
-interface UserDisplay {
-  name: string,
-  email: string,
-  role: string,
-}
+import UserEntryFormDialog from './components/user-entry-form-dialog'
+import { useDisplayUsers } from '@/lib/hooks';
 
 export default function Users() {
-  const { isPending, error, data, refetch, isSuccess } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => fetch('/api/users').then((res) => {
-      return res.json()
-    }).catch((err) => console.log(err)),
-    staleTime
-  });
-  const {
-    isPending: isPendingRole,
-    error: errorRole,
-    data: dataRole,
-    isSuccess: isSuccessRole } = useQuery<Role[], Error, RoleDisplayMap>({
-    queryKey: ['roles'],
-    queryFn: () => fetch('/api/roles')
-      .then((res) => res.json())
-      .catch((err) => console.log(err)),
-    select: (data) => {
-      let result: RoleDisplayMap = {};
-      data.forEach((role) => {
-        result[role.id] = {
-          value: role.id.toString(),
-          label: role.name
-        }
-      })
-      return result;
-    },
-    staleTime
-  });
-
-  const convertRoleIdToName = () => {
-    if (isSuccess && isSuccessRole) {
-      const result: UserDisplay[] = data.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          role: dataRole![user.roleId.toString()].label
-        }
-      })
-      return result;
-    } else {
-      return []
-    }
-  }
+  const { isPending, error, users, isFetching, refetch } = useDisplayUsers();
 
   return <div className="m-5 flex-1 flex flex-col gap-3 overflow-y-hidden">
     <div className="flex items-center justify-between">
       <h1 className="text-3xl font-bold">Users Manager</h1>
-      <FetchLoading isPending={isPendingRole} error={errorRole}>
-        <UserEntryFormDialog roles={dataRole!} refetch={refetch}/>
-      </FetchLoading>
+      <UserEntryFormDialog refetch={refetch}/>
     </div>
     <FetchLoading
       className="mx-auto mt-40 w-10 h-10"
-      isPending={isPending && isPendingRole} error={error}>
-      <DataTable columns={columns} data={convertRoleIdToName()} className="overflow-y-scroll"/>
+      isPending={isPending} error={error}>
+      <DataTable columns={columns} isLoading={isFetching} data={users} className="overflow-y-scroll"/>
     </FetchLoading>
   </div>
 }
