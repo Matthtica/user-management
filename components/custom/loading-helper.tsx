5cb1c0400374f5f27afbfcd0459203bbfd383981
Matthtica <zxnightstalker199733@gmail.com>
2024-03-06 15:06:07 +0630

    LoadingSpinner can be usable from outside as it is

--- components/custom/loading-helper.tsx
+++ components/custom/loading-helper.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import SVGSpinner from '~icons/svg-spinners/blocks-shuffle-2'
 
 interface LoadingSpinnerProps extends React.HTMLAttributes<SVGSVGElement> {}
-const LoadingSpinner: FC<LoadingSpinnerProps> = ({ ...props }: LoadingSpinnerProps) => {
+export const LoadingSpinner: FC<LoadingSpinnerProps> = ({ ...props }: LoadingSpinnerProps) => {
   return <SVGSpinner {...props}/>
 }
 
