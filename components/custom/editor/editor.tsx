4c4e962f7182d0b536660cf446346504bde210c1
Matthtica <zxnightstalker199733@gmail.com>
2024-03-12 15:27:41 +0000

    Editor

--- components/custom/editor/editor.tsx
+++ components/custom/editor/editor.tsx
@@ -1,59 +1,27 @@
-'use client'
-import React, { FC } from 'react'
-
-import { $getRoot, $getSelection } from 'lexical';
-
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import EditorContext from './context/editor-context';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import React from 'react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 
-const theme = {
-
+interface IEditorProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode
 }
 
-function onChange(editorState: any) {
-  editorState.read(() => {
-    const root = $getRoot();
-    const selection = $getSelection();
-    console.log(root, selection);
-  });
-}
-
-function MyCustomerAutoFocusPlugin() {
+export default function Editor({ children, className }: IEditorProps) {
   const [editor] = useLexicalComposerContext();
 
-  React.useEffect(() => {
-    editor.focus();
-  }, [editor]);
-
-  return null;
-}
-
-function onError(error: any) {
-  console.error(error);
-}
-
-
-export default function RTEditor() {
-  const initialConfig = {
-    namespace: 'MyEditor',
-    theme,
-    onError,
-  };
-
-  return <LexicalComposer initialConfig={initialConfig}>
-    <RichTextPlugin
-      contentEditable={<ContentEditable />}
-      placeholder={<div>Enter some text...</div>}
-      ErrorBoundary={LexicalErrorBoundary}
-    />
-    <OnChangePlugin onChange={onChange} />
-    <HistoryPlugin />
-    <MyCustomerAutoFocusPlugin />
-  </LexicalComposer>
-}
+  return (
+    <EditorContext.Provider
+      value={{ editor }}
+    >
+      {children}
+      <RichTextPlugin
+        contentEditable={<ContentEditable className={className} />}
+        placeholder={<div>placeholder...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+    </EditorContext.Provider>
+  );
+};
