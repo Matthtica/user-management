dee8b9a13955934e2f354c35cad6add7852439ca
Matthtica <zxnightstalker199733@gmail.com>
2024-03-13 09:31:25 +0000

    Changed loading helper icon from icons to lucide-react

--- components/custom/loading-helper.tsx
+++ components/custom/loading-helper.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react'
-import SVGSpinner from '~icons/svg-spinners/blocks-shuffle-2'
+import { Loader } from 'lucide-react'
 
-interface LoadingSpinnerProps extends React.HTMLAttributes<SVGSVGElement> {}
+interface LoadingSpinnerProps extends React.HTMLAttributes<SVGSVGElement> { }
 export const LoadingSpinner: FC<LoadingSpinnerProps> = ({ ...props }: LoadingSpinnerProps) => {
-  return <SVGSpinner {...props}/>
+  return <Loader {...props} />
 }
 
 interface FetchLoadingProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -12,7 +12,7 @@ interface FetchLoadingProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 // TODO: Error message red and big like serious error
-const FetchLoading: FC<FetchLoadingProps> = ( { isPending, error, children, className, ...props }) => {
+const FetchLoading: FC<FetchLoadingProps> = ({ isPending, error, children, className, ...props }) => {
   const LoadingComp = <LoadingSpinner className={className} />
   const ErrorComp = <div className={className} {...props}>{error?.message}</div>
 
@@ -22,7 +22,7 @@ const FetchLoading: FC<FetchLoadingProps> = ( { isPending, error, children, clas
 }
 
 const PageLoading: FC = () => {
-  return <LoadingSpinner className="w-10 h-10 mx-auto mt-[45%]"/>
+  return <LoadingSpinner className="w-10 h-10 mx-auto mt-[45%]" />
 }
 
 export {
