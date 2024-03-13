632fdbe6035c03bb65b99d12dcdefdb534b761e9
Matthtica <zxnightstalker199733@gmail.com>
2024-03-13 08:17:27 +0000

    Removed unplugin-icons configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
-import Icons from 'unplugin-icons/webpack'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.plugins.push(
-      Icons({
-        compiler: 'jsx',
-        jsx: 'react'
-      }),
-    )
-    return config
-  },
 };
 
 export default nextConfig;
