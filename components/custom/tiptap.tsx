ed31f74c60f30310795c3361f89160ebe239a007
Matthtica <zxnightstalker199733@gmail.com>
2024-03-14 08:33:29 +0000

    Added Strikethrough, underline, italic, bold, highlight, colorpicker, subscript, superscript

--- components/custom/tiptap.tsx
+++ components/custom/tiptap.tsx
@@ -7,28 +7,40 @@ import {
 import StarterKit from '@tiptap/starter-kit'
 import { Underline } from '@tiptap/extension-underline'
 import { Highlight } from '@tiptap/extension-highlight';
+import { Subscript } from '@tiptap/extension-subscript';
+import { Superscript } from '@tiptap/extension-superscript';
+import { TextStyle } from '@tiptap/extension-text-style';
+import { Color } from '@tiptap/extension-color';
 
 import { Button } from '../ui/button'
+import { HuePicker as ColorPicker } from 'react-color';
 import {
   Bold as BoldIc,
   Italic as ItalicIc,
   Underline as UnderlineIc,
   Strikethrough as StrikethroughIc,
   Code as CodeIc,
-  Highlighter as HighlighterIc
+  Highlighter as HighlighterIc,
+  Subscript as SubscriptIc,
+  Superscript as SuperscriptIc,
+  Palette
 } from 'lucide-react';
 import clsx from 'clsx';
+import VerticalDivider from './vertical-divider';
+import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
 
 // define your extension array
 const extensions = [
-  StarterKit, Underline, Highlight
+  StarterKit, Underline, Highlight,
+  Subscript, Superscript, TextStyle,
+  Color
 ]
 
 const content = '<p>Hello World!</p>'
 
 interface TiptapProps extends React.HTMLAttributes<HTMLDivElement> { }
 export default function Tiptap({ className }: TiptapProps) {
-  const menuClassNames = "rounded-md border-secondary border shadow-md p-1 bg-background";
+  const menuClassNames = "flex items-center rounded-md border-secondary border shadow-md p-1 bg-background";
   return <div className={className}>
     <EditorProvider extensions={extensions} content={content}>
       <FloatingMenu className={menuClassNames}><MenuBar /></FloatingMenu>
@@ -52,6 +64,7 @@ function MenuBar() {
   }
   return (
     <>
+      <VerticalDivider />
       <Button
         onClick={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
@@ -106,6 +119,31 @@ function MenuBar() {
       >
         <HighlighterIc {...iconProps} />
       </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleSubscript().run()}
+        disabled={!editor.can().chain().focus().toggleSubscript().run()}
+        size="icon"
+        variant="ghost"
+        className={clsx({ "opacity-60": !editor.isActive('subscript') }, btnClassNames)}
+      >
+        <SubscriptIc {...iconProps} />
+      </Button>
+      <Button
+        onClick={() => editor.chain().focus().toggleSuperscript().run()}
+        disabled={!editor.can().chain().focus().toggleSuperscript().run()}
+        size="icon"
+        variant="ghost"
+        className={clsx({ "opacity-60": !editor.isActive('superscript') }, btnClassNames)}
+      >
+        <SuperscriptIc {...iconProps} />
+      </Button>
+      <Popover>
+        <PopoverTrigger><Palette {...iconProps} /></PopoverTrigger>
+        <PopoverContent>
+          <ColorPicker onChange={(color, _) => editor.chain().focus().setColor(color.hex).run()} />
+        </PopoverContent>
+      </Popover>
+      <VerticalDivider />
     </>
   )
 }
