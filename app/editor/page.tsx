f318a4a2c6280dd553622efef6992fce1f922977
Matthtica <zxnightstalker199733@gmail.com>
2024-03-15 11:31:20 +0000

    Added style to both editor

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -3,8 +3,8 @@ import Tiptap from "@/components/custom/tiptap/tiptap"
 import NovelEditor from "@/components/custom/novel"
 
 export default function EditorPage() {
-  return <div>
-    <Tiptap className="" />
-    <NovelEditor className="" />
+  return <div className="flex flex-col gap-2">
+    <Tiptap className="bg-secondary rounded-md m-3 p-2 min-h-40 border" />
+    <NovelEditor className="bg-secondary rounded-md m-3 p-2 min-h-40 border" />
   </div>
 }
