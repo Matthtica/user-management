ccec21d303bc463a9fcb0942909c2c8af16d03d0
Matthtica <zxnightstalker199733@gmail.com>
2024-03-17 10:05:37 +0000

    Moved layout with side nav into workspace

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,65 +1,29 @@
 'use client'
-//import type { Metadata } from "next";
 import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from '@/components/theme-provider';
-import { SideDrawer, SideButton, SideButtonLabel, SideButtonIcon } from "@/components/custom/side-drawer";
-import { useRouter } from "next/navigation";
 import TanstackQueryClientProvider from "@/lib/tanstack-queryclient-provider";
 import { Toaster } from "@/components/ui/toaster";
-import { Home, Users, UserCog, PenTool } from 'lucide-react';
-import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
-
-interface RouteConfig {
-  text: string,
-  route: string,
-  icon: React.ReactElement
-}
-
-const iconProps = {
-  strokeWidth: "2px",
-  size: "1.5em"
-}
-
-const routes: RouteConfig[] = [
-  { text: "Home", route: '/', icon: <Home {...iconProps} /> },
-  { text: "User", route: '/users', icon: <Users {...iconProps} /> },
-  { text: "Role", route: '/roles', icon: <UserCog {...iconProps} /> },
-  { text: "Editor", route: '/editor', icon: <PenTool {...iconProps} /> },
-];
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const current_path = usePathname();
 
   return (
     <html lang="en">
-      <body className={`${inter.className} flex bg-primary-foreground h-screen`}>
+      <body className={`${inter.className} flex h-screen`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
           enableSystem
         >
           <TanstackQueryClientProvider>
-            <SideDrawer>
-              {routes.map((route) => (
-                <SideButton
-                  key={route.text}
-                  href={route.route}
-                  isCurrent={current_path === route.route}
-                >
-                  <SideButtonIcon>{route.icon}</SideButtonIcon>
-                  <SideButtonLabel>{route.text}</SideButtonLabel>
-                </SideButton>
-              ))}
-            </SideDrawer>
-            <div className="flex-1 m-2 bg-background rounded-md shadow-md flex flex-col">
+            <div className="w-full">
               {children}
             </div>
             <Toaster />
---
+++ app/workspace/layout.tsx
@@ -0,0 +1,52 @@
+'use client'
+import React from "react";
+import { ThemeProvider } from '@/components/theme-provider';
+import { SideDrawer, SideButton, SideButtonLabel, SideButtonIcon } from "@/components/custom/side-drawer";
+import TanstackQueryClientProvider from "@/lib/tanstack-queryclient-provider";
+import { Toaster } from "@/components/ui/toaster";
+import { Home, Users, UserCog, PenTool } from 'lucide-react';
+import { usePathname } from "next/navigation";
+
+interface RouteConfig {
+  text: string,
+  route: string,
+  icon: React.ReactElement
+}
+
+const iconProps = {
+  strokeWidth: "2px",
+  size: "1.5em"
+}
+
+const routes: RouteConfig[] = [
+  { text: "Home", route: '/', icon: <Home {...iconProps} /> },
+  { text: "User", route: '/workspace/users', icon: <Users {...iconProps} /> },
+  { text: "Role", route: '/workspace/roles', icon: <UserCog {...iconProps} /> },
+  { text: "Editor", route: '/workspace/editor', icon: <PenTool {...iconProps} /> },
+];
+
+export default function WorkspaceLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const current_path = usePathname();
+
+  return <div className="flex h-full">
+    <SideDrawer>
+      {routes.map((route) => (
+        <SideButton
+          key={route.text}
+          href={route.route}
+          isCurrent={current_path === route.route}
+        >
+          <SideButtonIcon>{route.icon}</SideButtonIcon>
+          <SideButtonLabel>{route.text}</SideButtonLabel>
+        </SideButton>
+      ))}
+    </SideDrawer>
+    <div className="flex-1 m-2 bg-background rounded-md shadow-md flex flex-col">
+      {children}
+    </div>
+  </div>
+}
