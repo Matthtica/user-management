c3e18707e4336e973f6c9ee488f6cda2a6ef8112
Matthtica <zxnightstalker199733@gmail.com>
2024-03-18 08:21:22 +0000

    Added user button from clerk and routes button properly settedup

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 'use client'
+import LinkButton from "@/components/custom/link-button";
 import ModeToggle from "@/components/custom/mode-toggle";
 import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { UserButton } from "@clerk/nextjs";
 import { LogInIcon } from "lucide-react";
 import Link from "next/link";
 
@@ -27,25 +27,26 @@ export default function Home() {
             <ModeToggle variant="ghost" />
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/auth/login" passHref>
+            <Link href="/sign-in" passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 <LogInIcon {...iconProps} />
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
+          <NavigationMenuItem>
+            <UserButton />
+          </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
     <div className="h-[88em] flex flex-col items-center gap-2">
-      <h1 className="text-8xl font-bold glow-text mt-28">
-        Pico SBS
+      <h1 className="text-8xl font-bold glow-text mt-28 uppercase">
+        silver
       </h1>
       <h2 className="text-xl">The most feature complete ERP solution, and it&apos;s cheap!!!</h2>
       <div className="flex gap-3 mt-5">
-        <Button asChild>
-          <Link href="/auth/signup">Sign up</Link>
-        </Button>
-        <Button variant="outline">Getting started</Button>
+        <LinkButton href="/sign-up">Sign up</LinkButton>
+        <LinkButton href="/workspace/roles" variant="outline">Getting started</LinkButton>
       </div>
     </div>
   </div>
