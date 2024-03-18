fc344b4838aa3ec710775dfa9e4a92374730ce74
Matthtica <zxnightstalker199733@gmail.com>
2024-03-18 05:28:43 +0000

    Combine query provider in the layout and use the clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,17 @@ import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from '@/components/theme-provider';
-import TanstackQueryClientProvider from "@/lib/tanstack-queryclient-provider";
 import { Toaster } from "@/components/ui/toaster";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
+
+const queryClient = new QueryClient()
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,21 +21,23 @@ export default function RootLayout({
 }>) {
 
   return (
-    <html lang="en">
-      <body className={`${inter.className} flex h-screen`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-        >
-          <TanstackQueryClientProvider>
-            <div className="w-full">
-              {children}
-            </div>
-            <Toaster />
-          </TanstackQueryClientProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} flex h-screen`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+          >
+            <QueryClientProvider client={queryClient}>
+              <div className="w-full">
+                {children}
+              </div>
+              <Toaster />
+            </QueryClientProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
