301b3d405c48e57bcba4fe0830b8f16b100dc150
Matthtica <zxnightstalker199733@gmail.com>
2024-03-19 04:25:27 +0000

    Sign in page and sign up page for clerk auth

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -11,8 +11,10 @@ export default function Page() {
         dividerText: "text-primary",
         formButtonPrimary: "bg-primary text-primary-foreground hover:bg-primary/90",
         formFieldLabel: "text-primary",
-        formFieldInput: "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        formFieldInput: "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-primary",
         footerActionText: "text-primary",
+        identityPreviewText: "text-primary",
+        formFieldInputShowPasswordButton: "text-primary"
       }
     }} />
   </div>
