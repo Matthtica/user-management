ca0cff610f19731cbe3db867e432c1e10d2b8b8e
Matthtica <zxnightstalker199733@gmail.com>
2024-03-19 04:25:43 +0000

    Dashboard page

---
+++ app/workspace/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage() {
+  return <div className="h-full flex items-center justify-center">
+    <h1 className="text-2xl text-center m-5">DashboardPage is not the main focus in development at the moment</h1>
+  </div>
+}
